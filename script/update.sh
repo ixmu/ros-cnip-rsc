b293ea36af3724757fd99ddd28d49b4dc0d61be5
marx peng <35778015+ixmu@users.noreply.github.com>
2024-02-07 00:10:11 +0800

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-update_ip (){
+update_ip () {
   curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/all_china.rsc
   curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/telecom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/telecom.rsc
   curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/unicom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/unicom.rsc
