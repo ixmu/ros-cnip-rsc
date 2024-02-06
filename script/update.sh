c7c33d792425c33527242c148529488798e04df6
marx peng <35778015+ixmu@users.noreply.github.com>
2024-02-07 00:00:32 +0800

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 hour=0
-timeout="2022-02-04"
+timeout="2024-02-04"
 # $(date -d "yesterday" "+%Y-%m-%d")
 while true; do
   if [ "$hour" -gt 24 ]; then
     break
   fi
 
-  status_code=$(curl -s -o /dev/null -w "%{http_code}" http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc)
+  status_code=$(curl -s -o /dev/null -w "%{http_code}" http://www.tcp5.com/list/$(date "+%Y.%m")/all_china${timeout}-${hour}.rsc)
 
   if [ "$status_code" -eq 200 ]; then
     break
@@ -21,12 +21,12 @@ if [ "$hour" -eq 25 ]; then
   exit 1
 fi
 
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/all_china.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/telecom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/telecom.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/unicom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/unicom.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/mobile$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/mobile.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/cernet$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/cernet.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/other$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/other.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/all_china${timeout}-${hour}.rsc > /tmp/all_china.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/telecom${timeout}-${hour}.rsc > /tmp/telecom.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/unicom${timeout}-${hour}.rsc > /tmp/unicom.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/mobile${timeout}-${hour}.rsc > /tmp/mobile.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/cernet${timeout}-${hour}.rsc > /tmp/cernet.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/other${timeout}-${hour}.rsc > /tmp/other.rsc
 
 cat /tmp/all_china.rsc > all_china.rsc
 cat /tmp/telecom.rsc > telecom.rsc
@@ -34,3 +34,4 @@ cat /tmp/unicom.rsc > unicom.rsc
 cat /tmp/mobile.rsc > mobile.rsc
 cat /tmp/cernet.rsc > cernet.rsc
 cat /tmp/other.rsc > other.rsc
+
