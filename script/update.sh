6c211f73cdcc9e754a910c71f4ace9cbb438657d
marx peng <35778015+ixmu@users.noreply.github.com>
2024-05-25 21:10:46 +0800

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -1,42 +1,13 @@
 #!/bin/bash
 
-update_ip () {
-  curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/all_china.rsc
-  curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/telecom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/telecom.rsc
-  curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/unicom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/unicom.rsc
-  curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/mobile$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/mobile.rsc
-  curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/cernet$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/cernet.rsc
-  curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/other$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > /tmp/other.rsc
-  rm all_china.rsc telecom.rsc unicom.rsc mobile.rsc cernet.rsc other.rsc
-  cat /tmp/all_china.rsc > all_china.rsc
-  cat /tmp/telecom.rsc > telecom.rsc
-  cat /tmp/unicom.rsc > unicom.rsc
-  cat /tmp/mobile.rsc > mobile.rsc
-  cat /tmp/cernet.rsc > cernet.rsc
-  cat /tmp/other.rsc > other.rsc
-}
-update_ip_v6 () {
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > all_china.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/telecom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > telecom.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/unicom$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > unicom.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/mobile$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > mobile.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/cernet$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > cernet.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/other$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc > other.rsc
+
+
   curl -kLfsm 5 https://raw.githubusercontent.com/ChanthMiao/China-IPv6-List/release/cn6.txt | sed -e 's/^/add address=/' -e 's/$/ comment=\"\" disabled\=no list\=List_ALL_China/'>/tmp/all_china_v6.rsc
   echo "/ipv6 firewall address-list" >all_china_v6.rsc
   cat /tmp/all_china_v6.rsc >> all_china_v6.rsc
-}
-hour=0
-while true; do
-  if [ "$hour" -gt 24 ]; then
-    break
-    exit 1
-  fi
-
-  status_code=$(curl -s -o /dev/null -w "%{http_code}" http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "yesterday" "+%Y-%m-%d")-${hour}.rsc)
-
-  if [ "$status_code" -eq 200 ]; then
-    update_ip
-    update_ip_v6
-    break
-  else
-    hour=$((hour + 1))
-  fi
-done
-
-
-
