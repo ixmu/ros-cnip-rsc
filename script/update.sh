c8355e3a6a39df8866705031b076eab51e4593e7
marx peng <35778015+ixmu@users.noreply.github.com>
2024-05-25 21:13:20 +0800

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "yesterday" "+%Y-%m-%d")-05.rsc > all_china.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/telecom$(date -d "yesterday" "+%Y-%m-%d")-05.rsc > telecom.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/unicom$(date -d "yesterday" "+%Y-%m-%d")-05.rsc > unicom.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/mobile$(date -d "yesterday" "+%Y-%m-%d")-05.rsc > mobile.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/cernet$(date -d "yesterday" "+%Y-%m-%d")-05.rsc > cernet.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/other$(date -d "yesterday" "+%Y-%m-%d")-05.rsc > other.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/all_china$(date -d "+%Y-%m-%d")-05.rsc > all_china.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/telecom$(date -d "+%Y-%m-%d")-05.rsc > telecom.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/unicom$(date -d "+%Y-%m-%d")-05.rsc > unicom.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/mobile$(date -d "+%Y-%m-%d")-05.rsc > mobile.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/cernet$(date -d "+%Y-%m-%d")-05.rsc > cernet.rsc
+curl -kLfsm 5 http://www.tcp5.com/list/$(date "+%Y.%m")/other$(date -d "+%Y-%m-%d")-05.rsc > other.rsc
 
 
 curl -kLfsm 5 https://raw.githubusercontent.com/ChanthMiao/China-IPv6-List/release/cn6.txt | sed -e 's/^/add address=/' -e 's/$/ comment=\"\" disabled\=no list\=List_ALL_China/'>/tmp/all_china_v6.rsc
