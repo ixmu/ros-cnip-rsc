ecf2f93f50fb1110539d27ed3a3147cb4ecb9279
marx peng <35778015+ixmu@users.noreply.github.com>
2024-07-10 00:43:25 +0800

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -4,12 +4,12 @@
 path=$(date "+%Y.%m")
 filename=$(date "+%Y-%m-%d")
 
-curl -kLfsm 5 http://www.tcp5.com/list/${path}/all_china${filename}-05.rsc > all_china.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/${path}/telecom${filename}-05.rsc > telecom.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/${path}/unicom${filename}-05.rsc > unicom.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/${path}/mobile${filename}-05.rsc > mobile.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/${path}/cernet${filename}-05.rsc > cernet.rsc
-curl -kLfsm 5 http://www.tcp5.com/list/${path}/other${filename}-05.rsc > other.rsc
+curl -kLfsm 5 http://ros.tcp5.com/list/${path}/all_china${filename}-05.rsc > all_china.rsc
+curl -kLfsm 5 http://ros.tcp5.com/list/${path}/telecom${filename}-05.rsc > telecom.rsc
+curl -kLfsm 5 http://ros.tcp5.com/list/${path}/unicom${filename}-05.rsc > unicom.rsc
+curl -kLfsm 5 http://ros.tcp5.com/list/${path}/mobile${filename}-05.rsc > mobile.rsc
+curl -kLfsm 5 http://ros.tcp5.com/list/${path}/cernet${filename}-05.rsc > cernet.rsc
+curl -kLfsm 5 http://ros.tcp5.com/list/${path}/other${filename}-05.rsc > other.rsc
 
 
 curl -kLfsm 5 https://raw.githubusercontent.com/ChanthMiao/China-IPv6-List/release/cn6.txt | sed -e 's/^/add address=/' -e 's/$/ comment=\"\" disabled\=no list\=List_ALL_China/'>/tmp/all_china_v6.rsc
