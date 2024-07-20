05d4ad1be432b6c2ca8d0618e2d2f0c7fe8569ff
marx peng <35778015+ixmu@users.noreply.github.com>
2024-07-20 20:49:03 +0800

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -4,6 +4,13 @@
 path=$(date "+%Y.%m")
 filename=$(date "+%Y-%m-%d")
 
+status_code=$(curl -s -o /dev/null -w "%{http_code}" "http://ros.tcp5.com/list/${path}/all_china${filename}-05.rsc")
+
+if [ $status_code -ne 200 ]; then
+    echo "非 200 状态码，退出程序"
+    exit 1
+fi
+
 curl -kLfsm 5 http://ros.tcp5.com/list/${path}/all_china${filename}-05.rsc > all_china.rsc
 curl -kLfsm 5 http://ros.tcp5.com/list/${path}/telecom${filename}-05.rsc > telecom.rsc
 curl -kLfsm 5 http://ros.tcp5.com/list/${path}/unicom${filename}-05.rsc > unicom.rsc
